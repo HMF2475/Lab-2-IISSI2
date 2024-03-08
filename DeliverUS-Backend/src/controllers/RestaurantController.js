3b26af924826e7ee7e69485c5e0a8064e6932d2b
Ivo Raimondi <raimondiivo@gmail.com>
2024-03-08 09:12:38 +0100

    Terminado

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -22,19 +22,77 @@ const index = async function (req, res) {
 // TODO: Complete the following functions
 
 const create = async function (req, res) {
-
+  try {
+    const newRestaurant = Restaurant.build(req.body)
+    // newRestaurant.userId = req.user.id // authenticated user
+    newRestaurant.userId = 1
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const show = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
+    }
+    )
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const update = async function (req, res) {
-
+  try {
+    await Restaurant.update(req.body, {
+      where: {
+        Id: req.params.restaurantId
+      }
+    })
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
+    }
+    )
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const destroy = async function (req, res) {
-
+  try {
+    const numDestruccion = await Restaurant.destroy({
+      where: {
+        Id: req.params.restaurantId
+      }
+    })
+    res.json(numDestruccion)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const RestaurantController = {
--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -1,7 +1,12 @@
 import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
-  // TODO: Include routes for restaurant described in the lab session README
-
+  app.route('/restaurants') // the endpoint path
+    .get(RestaurantController.index) // the function that will attend requests for that http verb and that path
+    .post(RestaurantController.create)
+  app.route('/restaurants/:restaurantId')
+    .get(RestaurantController.show)
+    .put(RestaurantController.update)
+    .delete(RestaurantController.destroy)
 }
 export default loadFileRoutes
